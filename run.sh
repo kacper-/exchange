3068a8df25b38c36352db3a9a7d27f3853f0e04b
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-02-01 22:56:04 +0100

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if test ! -t 0; then
-    input=$(java -jar target/exchange-0.0.1-SNAPSHOT-jar-with-dependencies.jar)
-    echo $input
-    echo $input | md5sum
+    INPUT=$(java -jar target/exchange-0.0.1-SNAPSHOT-jar-with-dependencies.jar)
+    echo "$INPUT"
+    echo "$INPUT" | md5sum
 else
     echo "No STDIN input"
 fi
